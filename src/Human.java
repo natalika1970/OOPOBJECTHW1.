70be364cb458ad1944a1cb2aff11e8f755017f6b
natalika1970 <natalika_70@mail.ru>
2022-10-20 23:42:53 +0300

    задание 1, 2 выполнены

---
+++ src/Human.java
@@ -0,0 +1,6 @@
+public class Human {
+    public int age;
+    public String name;
+    public String city;
+public String specialization;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       int currentYear = 2022;
+
+        Human maksim = new Human();
+        maksim.age = 35;
+        maksim.name = "Максим";
+        maksim.city = "Минск";
+        maksim.specialization = "бренд-менеджер";
+
+        Human anya = new Human();
+        anya.age = 29;
+        anya.name = "Аня";
+        anya.city = "Москва";
+        anya.specialization = "методист образовательных программ";
+
+        Human katya = new Human();
+        katya.age = 28;
+        katya.name = "Kaтя";
+        katya.city = "Калининград";
+        katya.specialization = "продакт-менеджер";
+
+        Human artem = new Human();
+        artem.age = 27;
+        artem.name = "Артем";
+        artem.city = "Москва";
+        artem.specialization = "директором по развитию бизнеса";
+
+        System.out.println("Привет! Меня зовут " +maksim.name+" Я из города " +maksim.city+
+                " Я родился в " +(currentYear - maksim.age) + " году. Я работаю на должности "+maksim.specialization+", Будем знакомы!");
+        System.out.println("Привет! Меня зовут " +anya.name+" Я из города " +anya.city+
+                " Я родилась в " +(currentYear - anya.age) + " году. Я работаю на должности "+anya.specialization+", Будем знакомы!");
+        System.out.println("Привет! Меня зовут " +katya.name+" Я из города " +katya.city+
+                " Я родилась в " +(currentYear - katya.age) + " году. Я работаю на должности "+katya.specialization+ ", Будем знакомы!");
+        System.out.println("Привет! Меня зовут " +artem.name+" Я из города " +artem.city+
+                " Я родился в " +(currentYear - artem.age) + " году. Я работаю на должности "+artem.specialization+ ", Будем знакомы!");
     }
 }
